ebf0420d03976adeeeab761bd9d05f1f91bc74f5
Abiola Martins <hsemartins@gmail.com>
2023-02-04 11:59:13 +0300

    Update CV file.

---
+++ index.js
@@ -0,0 +1,13 @@
+alert("Hello User, Click OK to Continue");
+// document.querySelector("h1").innerHTML="I am a skillful Web Developer";
+// document.querySelectorAll("h3")[1].style.color="red";
+document.querySelector("h1").style.color="red";
+document.querySelector("h1").style.alignContent="left";
+document.querySelector("h1").style.backgroundColor="yellow";
+document.querySelector("button").classList.add("invisible");
+document.querySelector("button").classList.toggle("invisible");
+// document.querySelector("h2").innerHTML="<em>Good Day</em>";
+// document.querySelector("h3").classList.add("huge");
+// document.querySelector("resume").classList.remove("invisible");
+// document.querySelector("h1").classList.toggle("invisible");
+
